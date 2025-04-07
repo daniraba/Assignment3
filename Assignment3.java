790f55059d18e4b95986a5c40e349024dfa371f0
daniraba <daniellarabayev@gmail.com>
2025-04-07 12:44:20 -0400

    naming

--- Assignment3.java
+++ Assignment3.java
@@ -12,45 +12,45 @@ public class Assignment3 {
         return length;
     }
 
-    public static int badness(String[] W, int i, int j, int width) {
+    public static int badness(String[] W, int i, int j, int w) {
         int lineLength = totalLength(W, i, j);
-        if (lineLength > width) {
+        if (lineLength > w) {
             return Integer.MAX_VALUE;
         } else {
-            int extraSpaces = width - lineLength;
+            int extraSpaces = w - lineLength;
             return extraSpaces * extraSpaces * extraSpaces;
         }
     }
 
-    public static int memoizedMinBadness(String[] W, int i, int[] memo, int[] lineBreaks, int width) {
+    public static int memoizedMinBadness(String[] W, int i, int[] memo, int[] linebreaks_memo, int w) {
         if (memo[i] >= 0) return memo[i];
         if (i == W.length) {
             memo[i] = 0;
-            lineBreaks[i] = W.length;
+            linebreaks_memo[i] = W.length;
             return 0;
         }
         int minBadness = Integer.MAX_VALUE;
         int indexOfMin = 0;
         for (int j = i + 1; j <= W.length; j++) {
-            int temp = badness(W, i, j, width);
+            int temp = badness(W, i, j, w);
             if (temp == Integer.MAX_VALUE) break;
-            temp += memoizedMinBadness(W, j, memo, lineBreaks, width);
+            temp += memoizedMinBadness(W, j, memo, linebreaks_memo, w);
             if (temp < minBadness) {
                 minBadness = temp;
                 indexOfMin = j;
             }
         }
         memo[i] = minBadness;
-        lineBreaks[i] = indexOfMin;
+        linebreaks_memo[i] = indexOfMin;
         return minBadness;
     }
 
-    public static List<Integer> split(String[] W, int width) {
+    public static List<Integer> split(String[] W, int w) {
         int n = W.length;
         int[] memo = new int[n + 1];
         int[] lineBreaks = new int[n + 1];
         Arrays.fill(memo, -1);
-        memoizedMinBadness(W, 0, memo, lineBreaks, width);
+        memoizedMinBadness(W, 0, memo, lineBreaks, w);
 
         List<Integer> breaks = new ArrayList<>();
         for (int i = 0; i < n; i = lineBreaks[i]) {
@@ -59,7 +59,7 @@ public class Assignment3 {
         return breaks;
     }
 
-    public static void justify(String[] W, int width, List<Integer> breaks, String filename) throws IOException {
+    public static void justify(String[] W, int w, List<Integer> breaks, String filename) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
         for (int i = 0; i < breaks.size(); i++) {
             int start = breaks.get(i);
@@ -68,7 +68,7 @@ public class Assignment3 {
             for (int j = start; j < end; j++) {
                 length += W[j].length();
             }
-            int spaces = width - length;
+            int spaces = w - length;
             int gaps = end - start - 1;
             StringBuilder line = new StringBuilder();
             for (int j = start; j < end; j++) {
